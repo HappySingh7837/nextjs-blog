bafff4c28466a56a9cb3ae2487d2385a0ef7e3bf
Raj Agrawal Mohit <rajagrawalmohit2@gmail.com>
2021-03-30 14:45:15 +0530

    Initial Setup

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,16 @@ import { Box, Typography, makeStyles, Button } from "@material-ui/core";
 import Head from "next/head";
 import { useEffect } from "react";
 
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { people: data }
+    }
+}
+
+
 const useStyles = makeStyles({
     text: {
         color: "#DC143C"
@@ -12,7 +22,7 @@ const useStyles = makeStyles({
     }
 });
 
-export default function Home() {
+const Home = ({ people }) => {
     const classes = useStyles()
     useEffect(() => {
         checkEnv();
@@ -26,7 +36,15 @@ export default function Home() {
             <Box>
                 <Typography className={classes.text}>Home</Typography>
                 <Button className={classes.button}>Secondary</Button>
+                {people.map(person => (
+                    <div key={person.id}>
+                        <h3>{person.name}</h3>
+                    </div>
+                )
+                )}
             </Box>
         </div>
-    );
+    )
 }
+
+export default Home
